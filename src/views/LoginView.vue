2d9d1ed424a3e4ac8950ba6c5c1328041e77655a
Alven Henas Orlando <109672644+Orlandd@users.noreply.github.com>
2025-03-01 16:21:28 +0700

    add: connect backend login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -10,11 +10,12 @@
     <div className="w-full md:w-1/2 flex items-center justify-center bg-white p-6 min-h-screen">
         <div className="w-full md:w-2/3">
             <h2 className="text-4xl font-semibold text-violet-900 text-center mb-6">LOGIN</h2>
-            <form>
+            <form @submit.prevent="login">
             <div className="mb-4">
                 <label className="block text-lg font-semibold text-violet-900 mb-2">Email</label>
                 <input 
                 type="email" 
+                v-model="email"
                 className="w-full p-2 border-3 border-yellow-500 rounded-full" 
                 placeholder="Enter your email"
                 />
@@ -24,6 +25,7 @@
                 <label className="block text-lg font-semibold text-violet-900 mb-2">Password</label>
                 <input 
                 type="password" 
+                v-model="password"
                 className="w-full p-2 border-3 border-yellow-500 rounded-full" 
                 placeholder="Enter your password"
                 />
@@ -46,10 +48,53 @@
 
 </template>
 <script>
+import axios from "axios";
+import router from '@/router';
+
 export default {
-    
-}
+    data() {
+        return {
+            email: '',
+            password: ''
+        };
+    },
+    methods: {
+        login() {
+            axios
+                .post('http://127.0.0.1:8000/api/auth/login', {
+                    email: this.email,
+                    password: this.password
+                })
+                .then((response) => {
+                    console.log(response);
+
+                    // Save user data to localStorage
+                    localStorage.setItem('email', response.data.data.email);
+                    localStorage.setItem('name', response.data.data.name);
+                    localStorage.setItem('role_id', response.data.data.role_id);
+                    localStorage.setItem('token', response.data.data.token);
+
+                    // Navigate to home page
+                    router.push({ name: 'home' });
+                })
+                .catch((error) => {
+                    // Handle errors
+                    if (error.response && error.response.data) {
+                        alert(error.response.data.message);
+                    } else {
+                        console.log('Error:', error.message);
+                    }
+                });
+        }
+    },
+    mounted() {
+        const username = localStorage.getItem('name');
+        if (username) {
+            router.push({ name: 'home' });
+        }
+    }
+};
 </script>
 <style lang="">
-    
+
 </style>
