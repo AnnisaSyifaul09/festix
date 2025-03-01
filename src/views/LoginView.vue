46e36fc47b4b3f0f8d9bf4634aea4a7e70da223a
AnnisaSyifaul09 <annisasyifaul78@gmail.com>
2025-03-01 11:32:44 +0700

    add: signin dan welcome

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -9,10 +9,10 @@
   </div>
     <div className="w-full md:w-1/2 flex items-center justify-center bg-white p-6 min-h-screen">
         <div className="w-full md:w-2/3">
-            <h2 className="text-4xl font-semibold text-violet-900 text-center mb-6">LOGIN</h2>
+            <h2 className="text-4xl font-semibold text-indigo-900 text-center mb-6">LOGIN</h2>
             <form>
             <div className="mb-4">
-                <label className="block text-lg font-semibold text-violet-900 mb-2">Email</label>
+                <label className="block text-lg font-medium text-indigo-900 mb-2">Email</label>
                 <input 
                 type="email" 
                 className="w-full p-2 border-3 border-yellow-500 rounded-full" 
@@ -21,7 +21,7 @@
             </div>
 
             <div className="mb-4">
-                <label className="block text-lg font-semibold text-violet-900 mb-2">Password</label>
+                <label className="block text-lg font-medium text-indigo-900 mb-2">Password</label>
                 <input 
                 type="password" 
                 className="w-full p-2 border-3 border-yellow-500 rounded-full" 
@@ -29,14 +29,13 @@
                 />
             </div>
 
-            <p className="text-sm text-violet-900 text-center mb-4">
-                Don't have an account? <a href="#" className="font-semibold">Sign In</a>
+            <p className="text-sm text-indigo-900 text-center mb-4">
+                Don't have an account? <a href="/signin" className="font-semibold">Sign In</a>
             </p>
 
             <button 
                 type="submit" 
-                className="w-full bg-yellow-500 text-white p-2 rounded-full text-lg font-semibold hover:bg-yellow-600 transition duration-300"
-            >
+                className="w-full bg-yellow-500 text-white p-2 rounded-full text-lg font-semibold hover:bg-yellow-600 transition duration-300">
                 Login
             </button>
             </form>
---
+++ src/views/SigninView.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+        <div class="flex flex-col md:flex-row h-screen">
+            <div class="w-full md:w-1/2 h-1/3 md:h-full hidden md:block">
+                <!-- <img
+                    src="/your-image.jpg"
+                    alt="Concert"
+                    class="w-full h-full object-cover"
+                /> -->
+            </div>
+            <div class="w-full md:w-1/2 flex items-center justify-center bg-white p-6 min-h-screen">
+                <div class="w-full md:w-2/3">
+                    <h2 class="text-4xl font-semibold text-indigo-900 text-center mb-6">SIGN IN</h2>
+                    <form>
+                        <div class="mb-4">
+                            <label class="block text-lg font-medium text-indigo-900 mb-2">Username</label>
+                            <input 
+                                type="text" 
+                                class="w-full p-2 border-3 border-yellow-500 rounded-full" 
+                                placeholder="Enter your username"
+                            />
+                        </div>
+                        <div class="mb-4">
+                            <label class="block text-lg font-medium text-indigo-900 mb-2">Name</label>
+                            <input 
+                                type="text" 
+                                class="w-full p-2 border-3 border-yellow-500 rounded-full" 
+                                placeholder="Enter your name"
+                            />
+                        </div>
+                        <div class="mb-4">
+                            <label class="block text-lg font-medium text-indigo-900 mb-2">Email</label>
+                            <input 
+                                type="email" 
+                                class="w-full p-2 border-3 border-yellow-500 rounded-full" 
+                                placeholder="Enter your email"
+                            />
+                        </div>
+                        <div class="mb-4">
+                            <label class="block text-lg font-medium text-indigo-900 mb-2">Password</label>
+                            <input 
+                                type="password" 
+                                class="w-full p-2 border-3 border-yellow-500 rounded-full" 
+                                placeholder="Enter your password"
+                            />
+                        </div>
+
+                        <p className="text-sm text-indigo-900 text-center mb-4">
+                            Have an account? <a href="/login" className="font-semibold">Log in</a>
+                        </p>
+
+                        <button 
+                            type="submit" 
+                            class="w-full bg-yellow-500 text-white p-2 rounded-full text-lg font-semibold hover:bg-yellow-600 transition duration-300">
+                            Sign In
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style>
+
+</style>
